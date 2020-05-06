7da23a27a972bf8f1ce23a9b729d6661cd3e93bd
JabariD <dennispayton15@gmail.com>
2020-05-06 08:12:47 -0500

    Updates

---
+++ precache-manifest.a876069346e68baecaf6c1d0997897e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681e422bd48c0a9c982519dcb8d4ec7b",
+    "url": "/React-Tutorial-First-Project-Robo-Friends-/index.html"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/React-Tutorial-First-Project-Robo-Friends-/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "b1cf7e3e3195a440463b",
+    "url": "/React-Tutorial-First-Project-Robo-Friends-/static/css/main.e68984b6.chunk.css"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/React-Tutorial-First-Project-Robo-Friends-/static/js/2.2b73e027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Tutorial-First-Project-Robo-Friends-/static/js/2.2b73e027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1cf7e3e3195a440463b",
+    "url": "/React-Tutorial-First-Project-Robo-Friends-/static/js/main.7dd02791.chunk.js"
+  },
+  {
+    "revision": "e6b0d27d3ad432f6109b",
+    "url": "/React-Tutorial-First-Project-Robo-Friends-/static/js/runtime-main.96c7b167.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/React-Tutorial-First-Project-Robo-Friends-/static/media/SEGA.12a3db8b.woff"
+  }
+]);
